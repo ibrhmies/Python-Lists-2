3c68120bc92bae0026acba3fe7ab9a4e851bf18c
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-10-31 18:27:52 +0300

    Python-Lists-2
    Python'da liste kuralları 2

---
+++ lists-2.py
@@ -0,0 +1,31 @@
+ulkeler = ['Türkiye','Almanya','Amerika','Rusya']
+
+sonuc = ulkeler
+#sonuc = type(ulkeler) -> ulkeler karakter dizisinin bize hangi tipte olduğunu verir.
+
+#sonuc = ulkeler[0:3] -> 0. kelimeden 3. kelimeye kadarki ulkeleri verir ama 3. kelimeyi vermez.
+
+#sonuc = ulkeler[-3:-1] -> -3. terimden -1. terime kadar alır ama -1 dahil değil.
+
+#ulkeler[1] = "Fransa" -> 1. terimi Fransa ile değiştirir.
+
+#sonuc = len(ulkeler) -> len komutuyla ulkeler listesinde kaç terim vardır bunu bize verir.
+
+#sonuc = ulkeler + ["Fransa","İngiltere"] -> ulkeler listesine Fransa ve İnglitere ekledik.
+
+#if Bloğu => koşul ifadeleri
+#if "Almanya" in ulkeler: -> eğer almanya ulkeler listesinin içindeyse true yazar.
+    #print("True")
+
+#Döngüler
+#for x in ulkeler:
+    #print(x)    
+#for komutuyla listenin içindeki her bir terimi yazdırdık fazla terimli listelerde for döngüsü işimize yarayacak.
+
+
+#del ulkeler[1] -> del komutuyla ulkeler listesinin içindeki 1. terimi sildik.
+#sonuc = ulkeler
+#print(sonuc)
+
+
+
